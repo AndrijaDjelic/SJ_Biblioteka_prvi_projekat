998a0226ed5dd7158c4449102ce71fe83cdb8428
andrijadjelic64 <andrijadjelic@gmail.com>
2022-01-16 04:46:19 +0100

    dodati html elementi

--- routes/rentbooksApi.js
+++ routes/rentbooksApi.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { sequelize, Books } = require('../models');
+const { sequelize, RentBooks, Books } = require('../models');
 const jwt = require('jsonwebtoken');
+const rentbooks = require('../models/rentbooks');
 require('dotenv').config();
 
 
@@ -11,18 +12,84 @@ route.use(express.urlencoded({ extended: true }));//kada budemo iz fron tend kom
 function authToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-  
+
     if (token == null) return res.status(401).json({ msg: "err" });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    
+
         if (err) return res.status(403).json({ msg: err });
-    
+
         req.user = user;
-    
+
         next();
     });
 }
-  route.use(authToken);
+route.use(authToken);
 
-  module.exports = route;
+route.get('/all', (req, res) => {
+    RentBooks.findAll(
+        {
+            include: [
+                { model: Books, attributes: ['id', 'title', 'author', 'genre'], as: 'book' }
+            ]
+        })
+        .then(rows => res.json(rows))
+        .catch(err => res.status(500).json(err));
+});
+
+route.get('/:id', (req, res) => {
+    RentBooks.findOne({
+        where: { id: req.params.id },
+        include: [
+            { model: Books, attributes: ['id', 'title', 'author', 'genre'], as: 'book' }
+        ]
+    })
+        .then(rows => res.json(rows))
+        .catch(err => res.status(500).json(err));
+
+});
+
+/*
+available: {
+      type:DataTypes.BOOLEAN,
+      allowNull: false
+}
+ this.belongsTo(Books, {foreignKey: 'bookId', as: 'book'});
+*/
+
+route.post('/', (req, res) => {
+    RentBooks.create({
+        available: req.body.available,
+        bookId: req.body.bookId
+    })
+        .then(rows => res.json(rows))
+        .catch(err => res.status(500).json(err));
+
+});
+
+route.put('/:id', (req, res) => {
+    RentBooks.findOne({ where: { id: req.params.id } })
+        .then(rentbooks => {
+            rentbooks.available = req.body.available,
+                rentbooks.bookId = req.body.bookId
+
+            rentbooks.save()
+                .then(rows => res.json(rows))
+                .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+
+});
+
+route.delete('/:id', (req, res) => {
+    RentBooks.findOne({ where: { id: req.params.id } })
+        .then(rentbooks => {
+            rentbooks.destroy()
+                .then(rows => res.json(rows))
+                .catch(err => res.status(500).json(err));
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = route;
